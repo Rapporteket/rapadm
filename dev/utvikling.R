821f2d4b66be641c5b8668b9409f492ec8dbf1b7
Arnfinn Hykkerud Steindal <arnfinn.hykkerud.steindal@helse-nord.no>
2025-08-25 16:22:53 +0200

    dev-greier

--- dev/utvikling.R
+++ dev/utvikling.R
@@ -1,9 +1,7 @@
 
 devtools::install("../rapbase/.")
+
 devtools::install(upgrade = FALSE)
 
 source("dev/env.R")
-
-Sys.setenv(MYSQL_HOST="localhost") # for mobilt kontor
-
 rapadm::run_app(browser = TRUE)
