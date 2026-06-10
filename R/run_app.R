deb1ccca181dc20f4c144918760b45d6e99c07b9
Arnfinn Hykkerud Steindal <arnfinn.steindal@gmail.com>
2026-06-10 13:39:44 +0200

    Bruk Alpine som os i docker image (#110)
    Færre eller ingen sårbarheter. Lettere.

--- R/run_app.R
+++ R/run_app.R
@@ -14,6 +14,11 @@ run_app <- function(browser = FALSE, logAsJson = FALSE) {
   if (browser) {
     options(shiny.launch.browser = TRUE)
   }
+
+  if (!capabilities()["cairo"]) {
+    knitr::opts_chunk$set(dev = "svglite") # To make it work without cairo.
+  }
+
   shiny::shinyApp(
     ui = app_ui,
     server = app_server
