298eb459753720f3bb270462bbe81df10c22b545
arnfinn <arnfinn.steindal@gmail.com>
2026-07-09 13:34:40 +0200

    @importFrom utils write.csv2
    + forenklet utils.R. Dropp eksport og masse doc.

--- R/utils.R
+++ R/utils.R
@@ -1,23 +1,4 @@
-#' Data pronouns
-#'
-#' See \code{rlang::\link[rlang:tidyeval-data]{.data}} for details
-#'
-#' @name .data
-#' @rdname pronoun
-#' @keywords internal
-#' @export
 #' @importFrom rlang .data
-#' @usage .data
-NULL
+#' @importFrom utils packageName write.csv2
 
-#' Utils utils
-#'
-#' See \code{utils::\link[utils:packageName]{packageName}} for details
-#'
-#' @name packageName
-#' @rdname packageName
-#' @keywords internal
-#' @export
-#' @importFrom utils packageName
-#' @usage packageName
 NULL
